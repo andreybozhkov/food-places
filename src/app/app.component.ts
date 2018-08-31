c27e91d93f41f39f5e31cdbd8784713a6e6509ff
andreybozhkov <bozhkov.andrey@gmail.com>
2018-08-31 14:01:28 +0300

    ADDED firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Food Places';
+
+  ngOnInit(): void {
+    let firebaseConfig = {
+      apiKey: "AIzaSyAQJ_rkFvEVYaeN9Uc_Fm6WbCbp8o-SW5Y",
+      authDomain: "food-places-andreybozhkov.firebaseapp.com",
+      databaseURL: "https://food-places-andreybozhkov.firebaseio.com/"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
